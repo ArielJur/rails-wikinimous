12e4f76cc4f7ab0869bb161f577d736fe60592c9
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-08-17 10:59:18 +0200

    Rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'article#update'
+  delete 'articles/:id', to: 'articles#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create!(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+end
+
+puts 'Seed finished'
+puts "#{Article.count} articles created"
